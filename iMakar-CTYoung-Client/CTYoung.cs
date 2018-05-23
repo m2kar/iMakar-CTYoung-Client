0ce39ec61de0a59438369d19d32dc52f2cb9d665
imakar <iMakar@qq.com>
2018-05-23 22:10:00 +0800

    add CTYoung.cs

---
+++ iMakar-CTYoung-Client/CTYoung.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMakar_CTYoung_Client
+{
+    class CTYoung
+    {
+        string id;
+        string passwd;
+        string ip;
+        CTYoung(string ip, string id, string passwd)
+        {
+            this.ip = ip;
+            this.id = id;
+            this.passwd = passwd;
+        }
+        static bool is_ip_valid(string ip)
+        {
+            //TODO complete this  
+            return true;
+        }
+        static string encrypt_passwd(string passwd)
+        {
+            //TODO encrypt passwd
+            return "";
+        }
+        void login()
+        {
+            //TODO
+        }
+        void logout()
+        {
+            //TODO
+        }
+
+    }
+}
